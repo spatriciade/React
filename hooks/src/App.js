25253e6e5a2c290424da2be4542e78531033a78b
spatriciade <spatriciade@hotmail.com>
2025-08-25 14:09:14 +0200

    Second Rocket

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -56,29 +56,60 @@ import "./App.css";
 // }
 
 // export default App;
-import { useState } from "react";
-import Rocket from "./components/Rocket";
+// import { useState } from "react";
+// import Rocket from "./components/Rocket";
+
+// function App() {
+//   const [lanzar, setLanzar] = useState(false);
+
+//   return (
+//     <div>
+//       <h1 style={{ textAlign: "center", marginTop: "20px" }}>Rocket Launch </h1>
+
+//       {!lanzar && (
+//         <button
+//           style={{ display: "block", margin: "20px auto", padding: "10px 20px" }}
+//           onClick={() => setLanzar(true)}
+//         >
+//           Count down!!!
+//         </button>
+//       )}
+
+//       <Rocket inicio={10} start={lanzar} />
+//     </div>
+//   );
+// }
+
+// export default App;
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [lanzar, setLanzar] = useState(false);
+  const [count, setCount] = useState(10);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => {
+        setCount(count - 1);
+      }, 1000);
+     
+    } else {
+      setRunning(true);
+    }
+  });
 
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Rocket Launch </h1>
-
-      {!lanzar && (
-        <button
-          style={{ display: "block", margin: "20px auto", padding: "10px 20px" }}
-          onClick={() => setLanzar(true)}
-        >
-          Count down!!!
-        </button>
-      )}
-
-      <Rocket inicio={10} start={lanzar} />
+      <h1>Rocket Launching - NASA Mars</h1>
+      <h2>Ignition Sequence Start</h2>
+      {running && <h3 className="pane red">All engines running. Lift Off!...</h3>}
+      <h3 className={count == 0 && "move"}>
+        <span className={count <= 5 && count >=3 ? "yellow" : count <=2 && "red"}>{count}</span>
+      
+      </h3>
     </div>
   );
 }
 
 export default App;
-
