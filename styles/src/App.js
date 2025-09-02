183d86d89d6c7aea3bdf45e49badef62e9ee8f7b
spatriciade <spatriciade@hotmail.com>
2025-09-02 13:20:41 +0200

    styled components ready

--- styles/src/App.js
+++ styles/src/App.js
@@ -1,24 +1,49 @@
-import styled from 'styled-components'; //importa la funcionalidad
+import styled from 'styled-components'; //importa la funcionalidad de la libreria
 
-const Title = styled.h1`
+const Title = styled.h1` //para trabajar a nivel de etiqueta 
 font-size:1.5em;
 text-align: center;
 color:AliceBlue;
 margin-bottom: 1rem;
 `;
 
-const Wrapper = styled.section`
-font-size: 1.5em;
-padding: 4em;
-background: DarkOliveGreen;
-text-align: center;
+// hereda estilos de Title
+const Paragraph = styled(Title)` 
+color:YellowGreen;
 
 `
 
+//para trabajar a nivel de etiqueta
+const Wrapper = styled.section.attrs(props =>({
+  id:"header",
+  size:props.size||"1.5em",
+}))` 
+font-size: ${props => props.size};
+padding: 4em;
+background: ${props => props.darkTheme ? "DarkSlateGrey" : "DarkOliveGreen"};
+text-align: center;
+
+&:hover h1 {
+color:Skyblue;}
+
+h1{
+font-size:1.5em;
+text-align: center;
+color:AliceBlue;
+margin-bottom: 1rem;
+}
+p{
+color:YellowGreen;
+}
+
+`
+
+
+ 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello, World!</Title>
+    <Wrapper darkTheme size="1.7em">
+      <h1>Hello, World!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Libero eum labore omnis aperiam eius fuga ratione suscipit! Officiis, non recusandae!
 
